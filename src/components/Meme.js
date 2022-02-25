885dc8a20afc8549e613d38f69a3d39c9e1f8c15
kushagrakrish <krishna619kk@gmail.com>
2022-02-25 23:41:43 +0530

    alt

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -60,7 +60,7 @@ const Meme = () => {
         </button>
       </div>
       <div className='meme'>
-        <img src={meme.randomImage} className='meme-image' />
+        <img src={meme.randomImage} className='meme-image' alt='' />
         <h2 className='meme-text top'>{meme.topText}</h2>
         <h2 className='meme-text bottom'>{meme.bottomText}</h2>
       </div>
